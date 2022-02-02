30049fe52c4232ff5fea79d9ec3a1779b9a7dc1f
khg <poohghg@naver.com>
2022-02-02 16:29:05 +0900

    ㅊㅊ

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import IndexPage from './pages/IndexPage';
 // 1 exact 을 설정하지 않아도됨
 // exact 안 쓰는 대신 /*가 필수입니다.
 // 2 switch -> routes로 변경
-// 3 component 옵션이 element로 변경
+// 3 component 옵션이 element로 변경 엘레멘트 유형으로 받아야함
 // 작은 번들사이즈를 유지
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<IndexPage />} />
+      <Route path="/*" element={<IndexPage />} />
       <Route path=":id" element={<DetailPage />} />
     </Routes>
   </Router>
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+type params = {
+  id: string;
+};
 
 const DetailPage = () => {
-  const test = '';
-  return <div>11 dasdsadasdsdsadas</div>;
+  const { id } = useParams<params>();
+  return <div>현재 ID는: {id}</div>;
 };
 
 export default DetailPage;
